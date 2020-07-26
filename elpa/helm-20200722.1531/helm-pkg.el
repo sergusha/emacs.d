1fdccf7a17f24a4a475144aa8914a53f3e7399b7
Sergey Medvedev <sergushamedvedev@gmail.com>
2020-07-26 19:26:29 +0300

    Adjust for mac more

---
+++ elpa/helm-20200722.1531/helm-pkg.el
@@ -0,0 +1,9 @@
+(define-package "helm" "20200722.1531" "Helm is an Emacs incremental and narrowing framework"
+  '((emacs "25.1")
+    (async "1.9.4")
+    (popup "0.5.3")
+    (helm-core "3.6.2"))
+  :commit "54a5e4cd7a10b2c285dc5bfe9dc100629b5d8b99" :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
---
+++ elpa/helm-core-20200726.943/helm-core-pkg.el
@@ -0,0 +1,7 @@
+(define-package "helm-core" "20200726.943" "Development files for Helm"
+  '((emacs "25.1")
+    (async "1.9.4"))
+  :commit "54a5e4cd7a10b2c285dc5bfe9dc100629b5d8b99" :url "https://emacs-helm.github.io/helm/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
